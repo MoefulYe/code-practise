2db06f160c534080674089de8a10770021a50a6d
ashenye <luren145@gmail.com>
2024-03-24 23:52:37 +0800

    x

--- type-challenges/medium/00008-readonly.ts
+++ type-challenges/medium/00008-readonly.ts
@@ -1,5 +1,15 @@
 /// url = https://tsch.js.org/8/play
 
-type MyReadOnly<Record, ReadOnlyKeys extends keyof Record> = {
-  [Key in keyof Record]: Key extends ReadOnlyKeys ? Readonly<Record[Key]> : Record[Key]
-}
+// type MyExclude<T, U> = T extends U ? never : T;
+
+// type MyReadonly<Record, ReadOnlyKeys extends keyof Record = keyof Record> = {
+//   readonly [Key in ReadOnlyKeys]: Record[Key];
+// } & {
+//   [Key in MyExclude<keyof Record, ReadOnlyKeys>]: Record[Key];
+// };
+
+type MyReadonly2<Record, ReadonlyKeys extends keyof Record = keyof Record> = {
+  readonly [ReadonlyKey in ReadonlyKeys]: Record[ReadonlyKey];
+} & {
+  [Key in keyof Record as Key extends ReadonlyKeys ? never : Key]: Record[Key];
+};
